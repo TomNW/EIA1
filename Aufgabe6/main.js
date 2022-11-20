2617fd8f607c53fa4e131b483293702c9486e37b
TomNW <tomnoahwiesmann@gmail.com>
2022-11-20 20:17:22 +0100

    b

--- Aufgabe6/main.js
+++ Aufgabe6/main.js
@@ -1,2 +1,6 @@
+const einwd = "82,5 Mio";
+const einwf = "67,0 Mio";
+const einwi = "60,6 Mio";
+const einwp = "38,0 Mio";
 console.log("Hallo Frankfurt!");
 //# sourceMappingURL=main.js.map
--- Aufgabe6/main.ts
+++ Aufgabe6/main.ts
@@ -1,1 +1,9 @@
+ const einwd = "82,5 Mio"
+ const einwf = "67,0 Mio"
+ const einwi = "60,6 Mio"
+ const einwp = "38,0 Mio"
+ 
+ 
+ 
+ 
  console.log("Hallo Frankfurt!");
