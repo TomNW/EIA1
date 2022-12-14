8dea9d962143715edbae572c0b490368ee722e8e
TomNW <tomnoahwiesmann@gmail.com>
2022-12-14 17:33:17 +0100

    remixbutton funkttioniert

--- Aufgabe9/main.ts
+++ Aufgabe9/main.ts
@@ -16,7 +16,6 @@ var beat: string [] = [Samples[4], Samples[5], Samples[8]];
 var zaehler: number = 0;
 var beatremix: number;
 var interval: number ;
-var interval2: number;
 var record = document.getElementById("record")
 
 
@@ -69,19 +68,7 @@ function StartBeat(): void {
   
 }
 
-function switchbuttonrmx(){
-    if (playbutton.style.visibility == "hidden")
-    {
-        playbutton.style.visibility = "hidden";
-        stopbutton.style.visibility = "visible";
-    }
-    else{
-        playbutton.style.visibility = "hidden";
-        stopbutton.style.visibility = "visible";
-    }
 
-}
-      
   
 
 function addClickListenersDrumPad(): void {
@@ -96,6 +83,4 @@ function addClickListenersDrumPad(): void {
         document.querySelector(".taste-8").addEventListener("click", function(): void {playSample(Samples[7]); });
         document.querySelector(".taste-9").addEventListener("click", function(): void {playSample(Samples[8]); });
         document.querySelector("#playbutton").addEventListener("click", StartBeat);
-        document.querySelector("#delete").addEventListener("click", function(): void {beat[]});
-       
 }
